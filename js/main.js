4fb84c505dc4b34679cbb171fa2dfe87f942e489
Ligaya_Zatoichi <braddahdave@arrayschool.com>
2016-11-15 17:01:25 -0700

    jQuery selector functions

--- js/main.js
+++ js/main.js
@@ -2,4 +2,24 @@
 
 $(document).ready(function(){
 
+
+  $('#fadeBox').click(function(){
+
+    $('#mainBox').fadeToggle(500);
+
+  });
+
+
+  $('#slideBox').click(function(){
+
+    $('#mainBox').slideToggle(500);
+
+  });
+
+  $('#mainBox').click(function(){
+
+    $('ul li:nth-child(2), #mainBox, #fadeBox, #slideBox, #container').fadeTo(500, 0.1);
+
+  });
+
 });
--- js/min/main-min.js
+++ js/min/main-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$("#fadeBox").click(function(){$("#mainBox").fadeToggle(500)}),$("#slideBox").click(function(){$("#mainBox").slideToggle(500)}),$("#mainBox").click(function(){$("ul li:nth-child(2), #mainBox, #fadeBox, #slideBox, #container").fadeTo(500,.1)})});
